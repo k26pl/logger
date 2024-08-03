8fc4436f52bd65d8b4f12e5dafb414e71a1d933a
k26 <k26@k26.pl>
2024-08-03 14:18:33 +0200

    Add license headers

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 export type Level = "verbose" | "info" | "log" | "warn" | "error";
 export type Log = {
   level: Level;
--- src/test.ts
+++ src/test.ts
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import { stringify_unknown } from "./core.js";
 import { fileDestination } from "./native.js";
 import { test } from "node:test";
